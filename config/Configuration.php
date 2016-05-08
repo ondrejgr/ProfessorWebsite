6db5c4ef7c735ef998373662a34732d2d65bc0b9
ondrej.gratz <ondrej.gratz@me.com>
2016-05-08 22:16:49 +0200

    Azure Configuration.php

--- config/Configuration.php
+++ config/Configuration.php
@@ -1,8 +1,9 @@
 <?php
 
+define('BASE_URL', 'http://gratzon.azurewebsites.net/');
 abstract class Configuration
 {
-    const ServerName = "localhost";
+    const ServerName = "eu-cdbr-azure-west-d.cloudapp.net";
     const DatabaseName = "gratzondb";
     
     const UserName = "ba072f630cdf08";
