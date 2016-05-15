5304b5b4d4602856a0c3dbbb8a7eb8108494e638
ondrej.gratz <ondrej.gratz@me.com>
2016-05-15 15:31:17 +0200

    remote config

--- config/Configuration.php
+++ config/Configuration.php
@@ -3,7 +3,7 @@
 define('BASE_URL', 'http://localhost/');
 abstract class Configuration
 {
-    const ServerName = "localhost";
+    const ServerName = "eu-cdbr-azure-west-d.cloudapp.net";
     const DatabaseName = "gratzondb";
     
     const UserName = "ba072f630cdf08";
